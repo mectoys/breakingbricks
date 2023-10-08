258ada0ce5028e615800321b8da51566b91369d0
mectoys <mectoys2013@gmail.com>
2023-10-07 22:46:08 -0500

    Video 3 : Agregando la esfera, movimiento en la pantala y colisión con la paleta.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
-screen = pygame.display.set_mode((1200, 600))
+screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Breaking Bricks -Rompiendo Ladrillos")
 
 # brick
@@ -29,6 +29,11 @@ for y in range(brick_rows):
 sphere = pygame.image.load('./images/sphere.png')
 sphere = sphere.convert_alpha()
 sphere_rect = sphere.get_rect()
+sphere_start = (350, 250)
+sphere_speed = (3.0, 3.0)
+sx, sy = sphere_speed
+sphere_served = False
+sphere_rect.topleft = sphere_start
 
 # paddle
 pad = pygame.image.load('./images/paddle.png')
@@ -44,8 +49,12 @@ while not game_over:
     # show bricks
     for br in bricks:
         screen.blit(brick, br)
+
     # show paddle
     screen.blit(pad, pad_rect)
+    # show sphere
+    screen.blit(sphere, sphere_rect)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
@@ -54,7 +63,39 @@ while not game_over:
         x -= 0.4 * dt
     if pressed[K_RIGHT]:
         x += 0.4 * dt
+    if pressed[K_SPACE]:
+        sphere_served = True
+
+    if pad_rect[0] + pad_rect.width >= sphere_rect[0] >= pad_rect[0] and \
+            sphere_rect[1] + sphere_rect.height >= pad_rect[1] and sy>0:
+        sy *= -1
+        continue
+
+    # Top
+    if sphere_rect[1] <= 0:
+        sphere_rect[1] = 0
+        sy *= -1
+
+    # Bottom
+    if sphere_rect[1] >= screen.get_height() - sphere_rect.height:
+        sphere_rect[1] = screen.get_height() - sphere_rect.height
+        sy *= -1
+
+    # Left
+    if sphere_rect[0] <= 0:
+        sphere_rect[0] = 0
+        sx *= -1
+
+    # Right
+    if sphere_rect[0] >= screen.get_width() - sphere_rect.width:
+        sphere_rect[0] = screen.get_width() - sphere_rect.width
+        sx *= -1
+
     pad_rect[0] = x
+    if sphere_served:
+        # movi sphere
+        sphere_rect[0] += sx
+        sphere_rect[1] += sy
 
     pygame.display.update()
 pygame.quit()
